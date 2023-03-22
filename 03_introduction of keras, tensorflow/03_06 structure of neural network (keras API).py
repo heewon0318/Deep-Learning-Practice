9e9627687fe16415d1e271283a196e40090ec16d
heewon0318 <heewonyoo0318@gmail.com>
2023-03-22 16:47:43 +0900

    신경망 구조: 핵심 Keras API 이해하기

--- 03_introduction of keras, tensorflow/03_06 structure of neural network (keras API).py
+++ 03_introduction of keras, tensorflow/03_06 structure of neural network (keras API).py
@@ -19,4 +19,27 @@ class SimpleDense(keras.layers.Layer):
             y = self.activation(y)
         return y
 
+import numpy as np
 
+model = keras.Sequential([keras.layers.Dense()])
+model.compile(optimizer= keras.optimizers.RMSprop(learning_rate= 0.1),
+              loss = keras.losses.MeanSquaredError(),
+              metrics=[keras.metrics.BinaryAccuracy()])
+
+indices_permutation = np.random.permutation(len(inputs))
+shuffled_inputs = inputs[indices_permutation]
+shuffled_targets = targets[indices_permutation]
+
+num_validation_samples = int(0.3 * len(inputs))
+val_inputs = shuffled_inputs[:num_validation_samples]
+val_targets = shuffled_targets[:num_validation_samples]
+training_inputs = shuffled_inputs[num_validation_samples : ]
+training_targets = shuffled_targets[num_validation_samples : ]
+model.fit(training_inputs,
+          training_targets,
+          epchs = 5,
+          batch_size= 16,
+          validation_data = (val_inputs, val_targets))
+
+predictions = model.predict(val_inputs, batch_size= 128)
+print(predictions[:10])
